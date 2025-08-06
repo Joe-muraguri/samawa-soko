e9d4c8dd572527baea51cf0e23306086fd4aabc3
Joe-muraguri <joe.crrm@gmail.com>
2025-08-06 17:20:45 +0300

    updated db for production

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'anothersecretkey')

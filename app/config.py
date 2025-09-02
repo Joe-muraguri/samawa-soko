96d88210795f4645f322d15db1efd3c623d4f54e
Joe-muraguri <joe.crrm@gmail.com>
2025-09-02 16:58:34 +0300

    updated endpoints for production

--- app/config.py
+++ app/config.py
@@ -6,11 +6,11 @@ load_dotenv()
 class Config:
 
     # 🚀 RENDER PRODUCTION DATABASE (Uncomment for deployment)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://', 1)
     
     
     # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/uza_db'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgresql://postgres:postgres@localhost:5432/uza_db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','postgresql://postgres:postgres@localhost:5432/uza_db')
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'anothersecretkey')
